2d9df9b54dd935c3fcf0d251eed5a99df7d23d15
Giovanni Ricca <giovanniricca@proton.me>
2025-06-07 20:38:37 +0200

    odin: Inherit from sm8350-common
    Change-Id: I231f337a47ac4fac86d2215da00ef96166b44317

---
+++ lineage_odin.mk
@@ -0,0 +1,19 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from odin device
+$(call inherit-product, device/xiaomi/odin/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := odin
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := 2106118C
+PRODUCT_NAME := lineage_odin
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
