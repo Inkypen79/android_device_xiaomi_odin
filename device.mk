8fef67f9590ef329e4a08bbc26901845f26b421e
Giovanni Ricca <giovanniricca@proton.me>
2025-06-20 17:35:52 +0200

    odin: rro_overlays: Import status bar configuration
    From OS2.0.1.0.UKBMIXM
    
    Change-Id: I9062e86ad06e5020122021d0a93cb6000ed45b1a

--- device.mk
+++ device.mk
@@ -10,6 +10,7 @@ $(call inherit-product, device/xiaomi/sm8350-common/common.mk)
 PRODUCT_PACKAGES += \
     FrameworkOverlayOdin \
     SettingsProviderOverlayOdin \
+    SystemUIOverlayOdin \
     WifiOverlayOdin
 
 PRODUCT_COPY_FILES += \
